5497c65f74e4530ed66f94ee3cda6536c939849b
RX_LOST <124406818+RX-LOST@users.noreply.github.com>
2026-04-28 12:34:06 -0700

    Update index.ts
    Remove nonexistent configs file import

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import storage from "./storage";
 import auth from "./auth";
-import configs from "./configs";
 
 const router = Router();
 
 router.use("/", storage);
-router.use("/", configs);
 router.use("/", auth);
 
 export default router;
