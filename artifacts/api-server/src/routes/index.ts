5b62cedeb8834e87b8358652070f5660dc096a4f
RX_LOST <124406818+RX-LOST@users.noreply.github.com>
2026-04-28 14:33:55 -0700

    Update index.ts
    debugging pass 1

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import storage from "./storage";
+import health from "./health";
 import auth from "./auth";
 import configs from "./configs";
 
 const router = Router();
 
 router.use("/", storage);
+router.use("/", health);
 router.use("/", configs);
 router.use("/", auth);
 
