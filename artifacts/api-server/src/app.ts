418229404162ad966bc45cdfe150f11d06b97d7c
RX_LOST <loganscolon@icloud.com>
2026-06-06 23:02:19 -0700

    Bug fix

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import fs from "node:fs";
+import path from "node:path";
 import { IS_PRODUCTION, FRONTEND_DIST } from "./config.js";
 import { errorHandler } from "./middleware/error.js";
 import routes from "./routes/index.js";
@@ -14,8 +15,8 @@ app.use("/api", routes);
 
 if (fs.existsSync(FRONTEND_DIST)) {
   app.use(express.static(FRONTEND_DIST));
-  app.get("*", (_req, res) => {
-    res.sendFile(FRONTEND_DIST + "/index.html");
+  app.get("/{*path}", (_req, res) => {
+    res.sendFile(path.join(FRONTEND_DIST, "index.html"));
   });
 }
 
